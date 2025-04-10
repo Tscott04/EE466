90930d13ad8bebce5413db0c04d0ae5aba92e1cf
Tscott04 <166546518+Tscott04@users.noreply.github.com>
2025-04-10 12:35:32 -0400

    attempt 1 at printing array

--- P3/intSort.asm
+++ P3/intSort.asm
@@ -1,57 +1,124 @@
-# intSort.s
-# Sorts an array of integers as WORD (4-bytes) and displays sorted array
-# Stack filter through all ints in array
-#---------------------------------------------------------
+.data
+myarray:    .word 4, 1, 3, 2, 16, 9, 10, 14, 8, 7
+arraysize:  .word 10
+newline:    .asciiz "\n"
+comma:      .asciiz ", "
+str1:       .asciiz "The original array is: "
+str2:       .asciiz "The sorted array is: "
 
-    .data
-myArray: .word 4, 1, 3, 2, 16, 9, 10, 14, 8, 7
-myArray_size: .word 10
-intro_msg: .asciiz "Your starting array is: " # Intro Header
-result_msg: .asciiz "The sorted arrayy is: " # Ending Header
-newline: .asciiz "\n"  # Newline character
-
-    .text
-    .globl main
+.text
+.globl main
 
 main:
-
-        li $v0, 4                      # Load syscall code for printing a string
-        la $a0, intro_msg              # Load address of intro_msg
-        syscall                        # Print intro_msg to show initial array
-
-        la $a0, myArray
-        lw $a1, myArray_size
-        jal array_print
-
-array_print:
-    add $t0, $zero, $zero      # i = 0
-
-print_loop:
-    beq $t0, $a1, print_done   # if i == size, done
-
-    sll $t1, $t0, 2            # offset = i * 4
-    add $t2, $a0, $t1          # address = base + offset
-    lw $t3, 0($t2)             # load value from array[i] into $t3
-
-    li $v0, 1                  # syscall to print int
-    move $a0, $t3              # move value into $a0
+    li $v0, 4
+    la $a0, str1
     syscall
 
+    jal print_array
+
+    jal sort
+
+    li $v0, 4
+    la $a0, str2
+    syscall
+
+    jal print_array
+
+    li $v0, 10
+    syscall
+
+####################################################
+# Print array procedure
+print_array:
+    addi $sp, $sp, -8
+    sw $ra, 4($sp)
+    sw $s0, 0($sp)
+
+    la $s0, myarray       # $s0 holds base address
+    lw $t0, arraysize     # size in $t0
+    li $t1, 0             # index = 0
+
+print_loop:
+    beq $t1, $t0, print_done
+
+    mul $t2, $t1, 4
+    add $t3, $s0, $t2
+    lw $a0, 0($t3)
+
+    li $v0, 1             # print integer
+    syscall
+
+    addi $t1, $t1, 1
+    beq $t1, $t0, print_loop
+
+    li $v0, 4             # print comma
+    la $a0, comma
+    syscall
+
+    j print_loop
+
+print_done:
     li $v0, 4
     la $a0, newline
     syscall
 
-    addi $t0, $t0, 1
-    j print_loop
-
-print_done:
+    lw $ra, 4($sp)
+    lw $s0, 0($sp)
+    addi $sp, $sp, 8
     jr $ra
 
+####################################################
+# Sort procedure (Bubble sort)
+sort:
+    addi $sp, $sp, -8
+    sw $ra, 4($sp)
+    sw $s0, 0($sp)
 
+    la $s0, myarray
+    lw $t6, arraysize
+    addi $t6, $t6, -1     # n-1 outer loops
 
+    li $t0, 0             # i = 0
+outer_loop:
+    beq $t0, $t6, sort_done
+    li $t1, 0             # j = 0
 
+inner_loop:
+    lw $t2, arraysize
+    addi $t2, $t2, -1
+    sub $t2, $t2, $t0
+    beq $t1, $t2, outer_next
 
+    la $t3, myarray
+    mul $t4, $t1, 4
+    add $t3, $t3, $t4
+    lw $t5, 0($t3)
+    lw $t7, 4($t3)
 
+    ble $t5, $t7, skip_swap
 
+    move $a0, $t3
+    jal swap
 
+skip_swap:
+    addi $t1, $t1, 1
+    j inner_loop
 
+outer_next:
+    addi $t0, $t0, 1
+    j outer_loop
+
+sort_done:
+    lw $ra, 4($sp)
+    lw $s0, 0($sp)
+    addi $sp, $sp, 8
+    jr $ra
+
+####################################################
+# Swap procedure
+swap:
+    lw $t0, 0($a0)
+    lw $t1, 4($a0)
+    sw $t1, 0($a0)
+    sw $t0, 4($a0)
+    jr $ra
